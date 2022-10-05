b84f0d0172486f5328c2c80cb06e59ad6eea9914
OwgerD <113381416+OwgerD@users.noreply.github.com>
2022-10-05 14:37:36 -0400

    Changes
    I made changes

--- src/ValidatePin/ValidatePin.java
+++ src/ValidatePin/ValidatePin.java
@@ -5,6 +5,31 @@ import java.util.Scanner;
 public class ValidatePin {
 
     public static void main(String[] args) {
-        
+     Scanner in = new Scanner(System.in);
+    	int pin = 4698;
+    	System.out.println("Enter PIN now.");
+    	int userPIN = in.nextInt();
+    	int userGuesses = 1;
+    	while (userPIN!=pin) {
+    		System.out.println("Invaild PIN, please enter correct PIN.");
+    		if (userGuesses>=3) {
+    			break;
+    		}
+    		userGuesses++;
+    		userPIN=in.nextInt();
+    		
+    	}
+    	if (userPIN!=pin) {
+    		System.out.println("Too many invailid answers! The police have been alerted to your location!");
+    	}else {
+    		System.out.println("Pin correct! Access granted!");
+    	}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
     }
 }
