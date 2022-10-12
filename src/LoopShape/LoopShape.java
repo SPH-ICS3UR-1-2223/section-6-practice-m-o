887641e9e5a820a6ae4cc5cb0d945781dc360435
OwgerD <113381416+OwgerD@users.noreply.github.com>
2022-10-12 14:39:12 -0400

    Update LoopShape.java

--- src/LoopShape/LoopShape.java
+++ src/LoopShape/LoopShape.java
@@ -4,7 +4,22 @@ public class LoopShape {
     
     static void createRectangle(int width, int height){
         //Draw a Rectangle
+    	int i;
+        for (i = 1; i <= width; i++) {
+            System.out.print("#");
+        }
+        System.out.println();
+        int repeats;
         
+       
+        for(repeats=1; repeats<height; repeats++) {
+        	System.out.println("#"+width-2+"#");
+        	
+        }
+        int u;
+        for (u = 1; u <= width; u++) {
+            System.out.print("#");
+        }
         
     }
     
